5e923f38ebce618ab334405fe286df1f04bfafb9
Jenaba Sow <jenabasow@gmail.com>
2020-02-13 23:09:58 -0500

    question 2

--- csci350.pl
+++ csci350.pl
@@ -10,3 +10,13 @@ sum-up-numbers-simple([H|T], N) :-
     N is H + Rem.
 sum-up-numbers-simple([_|T], N) :-
     sum-up-numbers-simple(T, N).  
+    
+/** Write a predicate sum-up-numbers-general(L, N). L is a list, which may
+contain as elements numbers and non-numbers. The predicate is true if N is the sum of all the
+numbers (including those in nested lists) in L. If there are no such numbers, the result is zero.**/
+
+sum-up-numbers-general([], 0). %check if head is list
+sum-up-numbers-general([H|T], N) :-
+    is_list(H),
+    sum-up-numbers-general(T, Remainder),
+    sum-up-numbers-general(H, HeadSum),
