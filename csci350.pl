4a05e271d09e8d1a567a9c792383841cd72dc1db
Jenaba Sow <jenabasow@gmail.com>
2020-02-13 22:39:15 -0500

    create csci350 and start question 1

---
+++ csci350.pl
@@ -0,0 +1,10 @@
+/** Write a predicate sum-up-numbers-simple(L, N). L is a list, which may
+contain as elements numbers and non-numbers. The predicate is true if N is the sum of the
+numbers not in nested lists in L. If there are no such numbers, the result is zero.
+**/
+
+sum-up-numbers-simple([], 0).  %default case, list is empty
+sum-up-numbers-simple([H|T], N) :- 
+    number(H),
+    sum-up-numbers-simple(T, Rem),
+    N is H + Rem.
